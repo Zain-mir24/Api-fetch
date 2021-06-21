90ebafbd53ec6cffda0ec6a9a590e141df729a25
Zain Mir <zainnaeemk10@gmail.com>
2021-06-21 14:24:18 +0500

    creatingscreen1

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { StyleSheet, Text, View } from 'react-native';
+import Screen1 from './components/Screen1';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <myDrawer />
     </View>
   );
 }
+const Stack = createStackNavigator();
+function myDrawer(){
+
+}
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/Screen1.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, Text, View } from 'react-native';
+
+function Screen1() {
+    return(
+        <View>
+            
+        </View>
+    )
+}
+
+export default Screen1
