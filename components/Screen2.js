6026cd0da6b4e91a58573f46bd66148c47ca45fa
Zain Mir <zainnaeemk10@gmail.com>
2021-06-21 20:06:48 +0500

    screen2 bottom navigator

--- components/Screen2.js
+++ components/Screen2.js
@@ -2,14 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, Text, View,FlatList } from "react-native";
-
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+const Tab = createMaterialBottomTabNavigator();
 function Screen2({navigation,route}) {
     const phone=route.params.Data
     return (
-        <View>
-        <Text>{phone}</Text>
-        </View>
+      <Tab.Navigator>
+
+          <Tab.Screen name="UserDetails"/>
+              </Tab.Navigator>
     )
 }
+
 
 export default Screen2
