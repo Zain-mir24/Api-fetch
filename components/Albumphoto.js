a9b48b32770d698ed91ae77ac612886836a293f6
Zain Mir <zainnaeemk10@gmail.com>
2021-06-23 15:51:10 +0500

    working fine

---
+++ components/Albumphoto.js
@@ -0,0 +1,24 @@
+
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right
+} from "native-base";
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+
+function Albumphoto() {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default Albumphoto
--- components/PostDetail.js
+++ components/PostDetail.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import {
     StyleSheet,
-   SafeAreaView,
+    SafeAreaView,
     View,
     FlatList,
     TouchableOpacity,
